bbfafa0576ec399045e52a90812fb19a196891ed
marktrr <thm2802@gmail.com>
2019-04-28 14:39:52 -0400

    Tic Tac Toe

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace HTranAssignment3
+{
+    partial class myForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTurn = new System.Windows.Forms.Label();
+            this.btReset = new System.Windows.Forms.Button();
+            this.pictureBox00 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox20 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox02 = new System.Windows.Forms.PictureBox();
+            this.pictureBox01 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(370, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "TURN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTurn
+            // 
+            this.labelTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTurn.ForeColor = System.Drawing.Color.Blue;
+            this.labelTurn.Location = new System.Drawing.Point(370, 88);
+            this.labelTurn.Name = "labelTurn";
+            this.labelTurn.Size = new System.Drawing.Size(152, 45);
+            this.labelTurn.TabIndex = 10;
+            this.labelTurn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btReset
+            // 
+            this.btReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btReset.ForeColor = System.Drawing.Color.Blue;
+            this.btReset.Location = new System.Drawing.Point(368, 173);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(153, 36);
+            this.btReset.TabIndex = 11;
+            this.btReset.Text = "RESET";
+            this.btReset.UseVisualStyleBackColor = true;
+            this.btReset.Click += new System.EventHandler(this.btReset_Click);
+            // 
+            // pictureBox00
+            // 
+            this.pictureBox00.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox00.Location = new System.Drawing.Point(51, 48);
+            this.pictureBox00.Name = "pictureBox00";
+            this.pictureBox00.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox00.TabIndex = 0;
+            this.pictureBox00.TabStop = false;
+            this.pictureBox00.Tag = "01";
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox12.Location = new System.Drawing.Point(223, 136);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox12.TabIndex = 13;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Tag = "06";
+            // 
+            // pictureBox20
+            // 
+            this.pictureBox20.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox20.Location = new System.Drawing.Point(51, 222);
+            this.pictureBox20.Name = "pictureBox20";
+            this.pictureBox20.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox20.TabIndex = 14;
+            this.pictureBox20.TabStop = false;
+            this.pictureBox20.Tag = "07";
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox11.Location = new System.Drawing.Point(137, 136);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox11.TabIndex = 15;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Tag = "05";
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox10.Location = new System.Drawing.Point(51, 136);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox10.TabIndex = 16;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Tag = "04";
+            // 
+            // pictureBox22
+            // 
+            this.pictureBox22.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox22.Location = new System.Drawing.Point(223, 222);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox22.TabIndex = 17;
+            this.pictureBox22.TabStop = false;
+            this.pictureBox22.Tag = "09";
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox21.Location = new System.Drawing.Point(137, 222);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox21.TabIndex = 18;
+            this.pictureBox21.TabStop = false;
+            this.pictureBox21.Tag = "08";
+            // 
+            // pictureBox02
+            // 
+            this.pictureBox02.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox02.Location = new System.Drawing.Point(223, 48);
+            this.pictureBox02.Name = "pictureBox02";
+            this.pictureBox02.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox02.TabIndex = 12;
+            this.pictureBox02.TabStop = false;
+            this.pictureBox02.Tag = "03";
+            // 
+            // pictureBox01
+            // 
+            this.pictureBox01.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox01.Location = new System.Drawing.Point(137, 48);
+            this.pictureBox01.Name = "pictureBox01";
+            this.pictureBox01.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox01.TabIndex = 19;
+            this.pictureBox01.TabStop = false;
+            this.pictureBox01.Tag = "02";
+            // 
+            // myForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 352);
+            this.Controls.Add(this.pictureBox01);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox22);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox20);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox02);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.labelTurn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox00);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "myForm";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.myForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox00;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTurn;
+        private System.Windows.Forms.Button btReset;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox20;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox22;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox02;
+        private System.Windows.Forms.PictureBox pictureBox01;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿/* Form1.cs 
+ * Assignment 3  
+ * Full Name: Hy Minh Tran 
+ * Student #: 7910276 
+ * Date created: 11/15/2018 
+ * Finished: 10:45 PM 11/15/2018.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTranAssignment3
+{
+    public partial class myForm : Form
+    {
+        public myForm()
+        {
+            InitializeComponent();       
+        }
+
+        PictureBox[] myList = new PictureBox[9]; // List of pictureBox to keep track all pictureBox.
+        private Boolean isX = true; // Check is X turn.
+
+        /// <summary>
+        /// Load form event. 
+        /// Assign the pictureBox to pictureBox list.
+        /// When pictureBox clicked. Run PBClick function.
+        /// </summary>
+        private void myForm_Load(object sender, EventArgs e)
+        {          
+            myList[0] = pictureBox00;
+            myList[1] = pictureBox01;
+            myList[2] = pictureBox02;
+            myList[3] = pictureBox10;
+            myList[4] = pictureBox11;
+            myList[5] = pictureBox12;
+            myList[6] = pictureBox20;
+            myList[7] = pictureBox21;
+            myList[8] = pictureBox22;
+
+            for (int i = 0; i < myList.Length; i++)
+            {
+                myList[i].Click += PBClick;
+            }
+            
+        }
+
+        /// <summary>
+        /// Assign the image X or O into pictureBox
+        /// After assign the value to pictureBox, check winners.
+        /// </summary>
+        private void PBClick(object sender, EventArgs e)
+        {         
+            PictureBox PB = (PictureBox)sender;
+           
+            if (isX && PB.Image == null)
+            {
+                labelTurn.Text = "O";
+                PB.Image = Properties.Resources.x;
+                PB.Tag = "X";
+                isX = false;
+            }
+            else if (!isX && PB.Image == null)
+            {
+                labelTurn.Text = "X";
+                PB.Image = Properties.Resources.o;
+                PB.Tag = "O";
+                isX = true;
+            }
+            else
+            {
+                MessageBox.Show("Already selected", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            checkWinners();
+        }
+
+        /// <summary>
+        /// Check winner conditions.
+        /// Example: Check row, column, diagonal.
+        /// </summary>
+        private void checkWinners()
+        {
+            isWin(myList[0],myList[1],myList[2]);
+            isWin(myList[3], myList[4], myList[5]);
+            isWin(myList[6], myList[7], myList[8]);
+            isWin(myList[0], myList[3], myList[6]);
+            isWin(myList[1], myList[4], myList[7]);
+            isWin(myList[2], myList[5], myList[8]);
+            isWin(myList[0], myList[4], myList[8]);
+            isWin(myList[2], myList[4], myList[6]);
+            if (isFull()) // Draw result.
+            {
+                MessageBox.Show("DRAW", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Reset();
+            }
+        }
+
+        /// <summary>
+        /// Method with 3 contructors. This method is check different kinds of win.
+        /// </summary>
+        private void isWin(PictureBox st, PictureBox nd, PictureBox rd)
+        {
+            if (st.Tag == nd.Tag && nd.Tag == rd.Tag)
+            {
+                XWin(st);
+                Reset();
+            }
+        }
+
+        /// <summary>
+        /// Restart the game so that another game can be played.
+        /// </summary>
+        private void Reset()
+        {
+            for(int i = 0; i < myList.Length; i++)
+            {
+                myList[i].Tag = "0"+i;
+                myList[i].Image = null;
+            }
+            labelTurn.Text = "";
+            isX = true;
+        }
+
+        /// <summary>
+        /// Function to display suitable message box to get player know who won.
+        /// </summary>
+        private void XWin(PictureBox pb)
+        {
+            if (pb.Tag == "X")
+            {
+                MessageBox.Show("X WIN", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Reset();
+            }
+            else
+            {
+                MessageBox.Show("O WIN", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Reset();
+            }
+        }
+
+        /// <summary>
+        /// for DRAW, check all pictureBox is already assigned with value.
+        /// </summary>
+        private Boolean isFull()
+        {
+            Boolean output = true;
+            for (int i = 0; i < myList.Length; i++)
+            {
+                if (myList[i].Image == null)
+                {
+                    output = false;
+                }                  
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// button reset.
+        /// </summary>
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+}
